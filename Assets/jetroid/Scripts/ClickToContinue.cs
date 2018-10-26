0241da1ece5b0448059611e3fbac138c3bc625e0
alexandre-w <alexandre.git67@gmail.com>
2018-10-27 10:54:46 +1100

    Splash Screen + icon

---
+++ Assets/jetroid/Scripts/ClickToContinue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickToContinue : MonoBehaviour {
+
+	public string scene ;
+
+	private bool loadLock ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0) && !loadLock){
+			LoadScene();
+		}
+	}
+
+    private void LoadScene()
+    {
+        loadLock = true; 
+		SceneManager.LoadScene(scene);
+
+    }
+}
